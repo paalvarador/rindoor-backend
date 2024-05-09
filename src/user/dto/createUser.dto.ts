43caad1a83aea2c3c743169900accc60dd859e5f
cos4h <olvadis2004@gmail.com>
2024-05-09 00:02:06 -0500

    added basic validations

--- src/user/dto/createUser.dto.ts
+++ src/user/dto/createUser.dto.ts
@@ -1,15 +1,33 @@
-import { Role } from "../entities/Role.enum";
+import { IsEmail, IsNotEmpty, IsString, MaxLength } from 'class-validator';
+import { Role } from '../entities/Role.enum';
 
 export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(50)
   firstName: string;
 
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(50)
   lastName: string;
 
+  @IsString()
+  @IsNotEmpty()
+  @IsEmail()
   email: string;
 
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(10)
   phone: string;
 
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(100)
   address: string;
 
+  @IsString()
+  @IsNotEmpty()
   role: Role.CLIENT | Role.ADMIN;
 }
--- src/user/dto/updateUser.dto.ts
+++ src/user/dto/updateUser.dto.ts
@@ -1,6 +1,10 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './createUser.dto';
+import { IsNumber, Max, Min } from 'class-validator';
 
 export class UpdateUserDto extends PartialType(CreateUserDto) {
+  @IsNumber()
+  @Min(1.0)
+  @Max(10.0)
   rating: number;
 }
