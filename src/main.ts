53bcc6ae880e6f64d13d1aaa823382c9e21ee26f
Pablo Alvarado <paalvarador@gmail.com>
2024-05-12 12:41:56 -0500

    Agregando CORS

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
   const configSwagger = new DocumentBuilder()
     .setTitle('Rindoor API')
     .setDescription(
