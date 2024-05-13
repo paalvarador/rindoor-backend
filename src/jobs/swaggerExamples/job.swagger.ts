e75d99192d04d0c90870c45d499286757a4f9e08
cos4h <olvadis2004@gmail.com>
2024-05-12 19:10:17 -0500

    added filter jobs

--- src/jobs/swaggerExamples/job.swagger.ts
+++ src/jobs/swaggerExamples/job.swagger.ts
@@ -6,3 +6,29 @@ export const exampleCreatedJob = {
   categoryId: 'cfc48688-57ca-43d2-9f1d-88c8a236aefe',
   userId: '18908870-475e-42e1-9620-588c38221377',
 };
+
+export const jobApiBody = {
+  schema: {
+    type: 'object',
+    properties: {
+      file: { type: 'string', format: 'binary' },
+      name: { type: 'string', description: 'Nombre del trabajo' },
+      description: {
+        type: 'string',
+        description: 'Breve descripcion del trabajo',
+      },
+      base_price: {
+        type: 'number',
+        description: 'Precio base de postulacion',
+      },
+      categoryId: {
+        type: 'string',
+        description: 'Categoria del trabajo',
+      },
+      userId: {
+        type: 'string',
+        description: 'ID de Usuario',
+      },
+    },
+  },
+};
