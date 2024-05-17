44177b4537669244685ffc936d6baa68fc31248d
cos4h <olvadis2004@gmail.com>
2024-05-17 16:28:27 -0500

    Filtro de job por ubicacion, ordenamiento por precio, ultimo y nombre del trabajo

--- src/dto/filterJob.dto.ts
+++ src/dto/filterJob.dto.ts
@@ -9,6 +9,9 @@ import {
 import { PaginationQuery } from './pagintation.dto';
 import { Transform } from 'class-transformer';
 import { ApiPropertyOptional } from '@nestjs/swagger';
+import { IsValidCity } from 'src/decorators/isValidCity';
+import { IsValidProvince } from 'src/decorators/isValidProvince';
+import { IsValidCountry } from 'src/decorators/isValidCountry';
 
 export class filterJobCategory extends PaginationQuery {
   @ApiPropertyOptional({
@@ -39,4 +42,79 @@ export class filterJobCategory extends PaginationQuery {
   @Max(999999999.99)
   @Transform(({ value }) => Number(value))
   maxPrice?: number;
+
+  /**
+   * @example 11
+   * @description Pais del usuario
+   */
+  @IsOptional()
+  @IsNumber()
+  @Min(1)
+  @Max(99999)
+  @IsValidCountry({
+    message: 'Pais invalido',
+  })
+  @Transform(({ value }) => Number(value))
+  country: number;
+
+  /**
+   * @example 3656
+   * @description id de la provincia del usuario
+   */
+  @IsOptional()
+  @IsNumber()
+  @Min(1)
+  @Max(99999)
+  @IsValidProvince('country', {
+    message: 'Provincia invalida',
+  })
+  @Transform(({ value }) => Number(value))
+  province: number;
+
+  /**
+   * @example 682
+   * @description Id de la ciudad del usuario
+   */
+  @IsOptional()
+  @IsNumber()
+  @Min(1)
+  @Max(99999)
+  @IsValidCity('country', 'province', {
+    message: 'Ciudad invalida',
+  })
+  @Transform(({ value }) => Number(value))
+  city: number;
+
+  /**
+   * @example 1
+   * @description Orderna de forma ascendente(0) o descendente(1)
+   */
+  @IsOptional()
+  @IsNumber()
+  @Min(0)
+  @Max(1)
+  @Transform(({ value }) => Number(value))
+  name: number;
+
+  /**
+   * @example 1
+   * @description Orderna mayores precios(0) o menores precios(1)
+   */
+  @IsOptional()
+  @IsNumber()
+  @Min(0)
+  @Max(1)
+  @Transform(({ value }) => Number(value))
+  prices: number;
+
+  /**
+   * @example 1
+   * @description Orderna los trabajos mas recientes(0) o los mas antiguos(1)
+   */
+  @IsOptional()
+  @IsNumber()
+  @Min(0)
+  @Max(1)
+  @Transform(({ value }) => Number(value))
+  latest: number;
 }
