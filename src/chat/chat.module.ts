31dc900597154b42a3ff9f22879d33bd07dc62d1
Pablo Alvarado <paalvarador@gmail.com>
2024-05-22 08:44:33 -0500

    Trabajado en el modulo de chat

--- src/chat/chat.module.ts
+++ src/chat/chat.module.ts
@@ -3,9 +3,11 @@ import { ChatService } from './chat.service';
 import { ChatGateway } from './chat.gateway';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Chat } from './entities/chat.entity';
+import { ChatController } from './chat.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Chat])],
   providers: [ChatGateway, ChatService],
+  controllers: [ChatController],
 })
 export class ChatModule {}
